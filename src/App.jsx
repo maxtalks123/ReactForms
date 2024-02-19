edf4dbc73f808dee1bdcca10c9bc92675dd50d95
Max Emerson <maxemerson2013@gmail.com>
2024-02-19 14:25:08 +0000

    finished workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+  });
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("The form values are", formValues);
+  }
+  function handleInputChange(event) {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={formValues.name}
+        onChange={handleInputChange}
+      />
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={formValues.email}
+        onChange={handleInputChange}
+      />
+      <p>Current value is: {formValues.name}</p>
+      <p>Current value is: {formValues.email}</p>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
-
-export default App
